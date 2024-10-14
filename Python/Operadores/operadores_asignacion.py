4bb800ad866563ff7a95828c35fbcbc1c4bbbc3f
Nicolasviruel <nicolasviruel@gmail.com>
2024-10-14 14:19:45 -0300

    Operadores de asignacion

---
+++ Python/Operadores/operadores_asignacion.py
@@ -0,0 +1,19 @@
+print('**** Operadores de asignacion ****')
+
+numero = 5
+print(f'Valor de numero: {numero}')
+numero = 10
+print(f'Valor de numero: {numero}')
+
+cadena = "Saludos desde Python"
+print(f'Valor de la cadena: {cadena}')
+
+# Asignacion Multiple
+
+x, y ,z = 5, "hola", -9.15
+print(f'\n Valor de X: {x}, valor de Y: {y}, valor de Z: {z}')
+
+# Asignacion Encadenada
+
+a = b = c = 10
+print(f'\n Valor a = {a}, valor de b = {b}, valor de c = {c}')
